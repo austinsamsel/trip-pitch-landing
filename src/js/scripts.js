829cf74baea69b4936a3125d4cb6ea5685101445
austin samsel <austinsamsel@gmail.com>
2016-01-07 21:43:02 -0500

    fine tune menu and sections for mobile

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,10 +2,11 @@
 
 (function ($, window, document, undefined) {
 
-  // fix menu when passed
+    // fix menu when passed
     $('.masthead')
       .visibility({
         once: false,
+        offset:100,
         onBottomPassed: function() {
           $('.fixed.menu').transition('fade in');
         },
@@ -15,9 +16,46 @@
       })
     ;
 
-    // create sidebar and attach to menu open
-    $('.ui.sidebar')
-      .sidebar('attach events', '.toc.item')
-    ;
+    $('#menunav').onePageNav({
+        currentClass: 'active',
+        changeHash: true,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        filter: '',
+        easing: 'swing',
+        begin: function() {
+            //I get fired when the animation is starting
+            console.log('start');
+        },
+        end: function() {
+            //I get fired when the animation is ending
+            console.log('fin');
+        },
+        scrollChange: function($currentListItem) {
+            //I get fired when you enter a section and I pass the list item of the section
+            console.log('new section')
+        }
+    });
+
+    $('#topmenu').onePageNav({
+        currentClass: 'active',
+        changeHash: true,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        filter: '',
+        easing: 'swing',
+        begin: function() {
+            //I get fired when the animation is starting
+            console.log('start');
+        },
+        end: function() {
+            //I get fired when the animation is ending
+            console.log('fin');
+        },
+        scrollChange: function($currentListItem) {
+            //I get fired when you enter a section and I pass the list item of the section
+            console.log('new section')
+        }
+    });
 
 })(jQuery, window, document);
